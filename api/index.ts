60badf23ea4540150f0b3c965361ea9639194ddf
Allan George <143169885+Allan13062@users.noreply.github.com>
2026-07-04 16:20:31 +0300

    fix: revert api/index.ts to static import (dynamic import breaks Vercel bundling)

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,3 @@
-let app: any;
-let startupError: any;
+import app from "../server";
 
-try {
-  const mod = await import("../server");
-  app = mod.default;
-} catch (err: any) {
-  startupError = err;
-}
-
-export default function handler(req: any, res: any) {
-  if (startupError) {
-    return res.status(500).json({
-      error: "Server failed to start",
-      message: startupError.message,
-      stack: startupError.stack?.split("\n").slice(0, 8).join("\n")
-    });
-  }
-  return app(req, res);
-}
+export default app;
