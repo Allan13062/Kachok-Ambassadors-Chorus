76a0945b560f5e8234e3c5994eadbfb213993814
Allan George <143169885+Allan13062@users.noreply.github.com>
2026-07-06 09:22:39 +0300

    Delete fix-values.cjs

--- fix-values.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = 'src/components/AdminPanel.tsx';
-let content = fs.readFileSync(path, 'utf8');
-
-const regex = /value=\{([a-zA-Z0-9]+Form\.[a-zA-Z0-9]+)\}/g;
-content = content.replace(regex, 'value={$1 || ""}');
-
-fs.writeFileSync(path, content, 'utf8');
