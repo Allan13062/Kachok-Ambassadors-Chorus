604ef69dc8fa473478f6da39f4dc51c227520aba
Allan George <143169885+Allan13062@users.noreply.github.com>
2026-07-06 09:23:46 +0300

    Delete fix-music.cjs

--- fix-music.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = 'src/components/MusicStreaming.tsx';
-let content = fs.readFileSync(path, 'utf8');
-
-content = content.replace(/value=\{currentTime\}/g, 'value={currentTime || 0}');
-
-fs.writeFileSync(path, content, 'utf8');
