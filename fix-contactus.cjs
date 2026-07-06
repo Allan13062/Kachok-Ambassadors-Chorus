9f1cfa4605b34f75851d1e50395874ff027a9cb2
Allan George <143169885+Allan13062@users.noreply.github.com>
2026-07-06 09:23:00 +0300

    Delete fix-contactus.cjs

--- fix-contactus.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = 'src/components/ContactUs.tsx';
-let content = fs.readFileSync(path, 'utf8');
-
-const regex = /value=\{formData\.([a-zA-Z0-9]+)\}/g;
-content = content.replace(regex, 'value={formData.$1 || ""}');
-
-fs.writeFileSync(path, content, 'utf8');
